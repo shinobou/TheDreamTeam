7fd8a06b036c76afb2c415120245bfb26d8eb184
Joyce <fernandezjoyce331@gmail.com>
2026-01-19 11:17:59 +0800

    Student5 Commit

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -18,6 +18,11 @@ android {
         versionName = "1.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+        externalNativeBuild {
+            cmake {
+                cppFlags += ""
+            }
+        }
     }
 
     buildTypes {
@@ -38,6 +43,12 @@ android {
     }
     buildFeatures {
         compose = true
+    }
+    externalNativeBuild {
+        cmake {
+            path = file("src/main/cpp/CMakeLists.txt")
+            version = "3.22.1"
+        }
     }
 }
 
--- app/src/main/java/com/example/thedreamteam/StudentFiveActivity.kt
+++ app/src/main/java/com/example/thedreamteam/StudentFiveActivity.kt
@@ -1,12 +1,20 @@
-package com.example.thedreamteam;
+package com.example.thedreamteam
 
 import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
 
-class StudentTwoActivity : AppCompatActivity() {
-
+class StudentFiveActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_student_two)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_student_five)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
     }
 }
